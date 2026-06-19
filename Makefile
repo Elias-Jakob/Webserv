cb2c02e603d9e97a9f380a38d1a36b963bdefe04
Elias Jakob <ejakob@c2r7p5.42vienna.com>
2026-06-19 17:39:30 +0200

    feat: remove useless printSocketInfo helper function

--- Makefile
+++ Makefile
@@ -12,7 +12,6 @@ SRC = src/http/ClientConnection.cpp \
 		src/parsers/MultipartParser.cpp \
 		src/server/Server.cpp \
 		src/server/ServerEventLoop.cpp \
-		src/server/DebugHelper.cpp \
 		src/main.cpp
 
 OBJDIR = obj
--- src/server/DebugHelper.cpp
+++ src/server/DebugHelper.cpp
@@ -1,27 +1,2 @@
 # include "Server.hpp"
-# include <arpa/inet.h>
 
-void	printSocketInfo(int sockfd)
-{
-    sockaddr_in addr;
-    socklen_t len = sizeof(addr);
-
-    if (getsockname(sockfd, reinterpret_cast<sockaddr*>(&addr), &len) == -1)
-    {
-				std::cout << "Helper function: " << std::strerror(errno) << std::endl;
-        return;
-    }
-
-    char ip[INET_ADDRSTRLEN];
-    if (inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip)) == NULL)
-    {
-				std::cout << "Helper function: " << std::strerror(errno) << std::endl;
-        return;
-    }
-
-    std::cout << "Local address: "
-              << ip
-              << ":"
-              << ntohs(addr.sin_port)
-              << std::endl;
-}
