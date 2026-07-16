e4be32268be8ed8db2363550bec4b96270094ba6
Elias Jakob <elias5.jakob@gmail.com>
2026-07-16 14:29:45 +0200

    test: add cgi tests

--- www/cgi-bin/script.php
+++ www/cgi-bin/script.php
@@ -1,4 +1,13 @@
-#!/usr/bin/php-cgi
+#!/usr/bin/env php-cgi
 <?php
-echo "Hee";
-?>
+
+header("Content-Type: text/html");
+
+echo "PHP-CGI funktioniert!\n\n";
+
+echo "PHP Version: " . PHP_VERSION . "\n";
+echo "SAPI: " . PHP_SAPI . "\n\n";
+
+echo "REQUEST_METHOD: " . ($_SERVER['REQUEST_METHOD'] ?? '-') . "\n";
+echo "QUERY_STRING: " . ($_SERVER['QUERY_STRING'] ?? '-') . "\n";
+echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? '-') . "\n";
