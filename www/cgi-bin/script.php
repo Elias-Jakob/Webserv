eb3d6f23829e70a552f440bb4665d93bd2fc38b4
Elias Jakob <elias5.jakob@gmail.com>
2026-07-21 14:53:05 +0200

    tests: add headers to the cgi test scripts

--- www/cgi-bin/script.php
+++ www/cgi-bin/script.php
@@ -3,11 +3,9 @@
 
 header("Content-Type: text/html");
 
-echo "PHP-CGI funktioniert!\n\n";
-
-echo "PHP Version: " . PHP_VERSION . "\n";
-echo "SAPI: " . PHP_SAPI . "\n\n";
-
-echo "REQUEST_METHOD: " . ($_SERVER['REQUEST_METHOD'] ?? '-') . "\n";
-echo "QUERY_STRING: " . ($_SERVER['QUERY_STRING'] ?? '-') . "\n";
-echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? '-') . "\n";
+echo "PHP-CGI funktioniert!\n\r";
+echo "PHP Version: " . PHP_VERSION . "\n\r";
+echo "SAPI: " . PHP_SAPI . "\n\r";
+echo "REQUEST_METHOD: " . ($_SERVER['REQUEST_METHOD'] ?? '-') . "\n\r";
+echo "QUERY_STRING: " . ($_SERVER['QUERY_STRING'] ?? '-') . "\n\r";
+echo "SCRIPT_NAME: " . ($_SERVER['SCRIPT_NAME'] ?? '-') . "\n\r";
--- www/cgi-bin/script.py
+++ www/cgi-bin/script.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import os
 
+print("Status: 404 Not Found", end="\r\n");
+print("CONTENT-TYPE: text/html", end="\r\n");
+print("ANOTHER-HEADER: hello!!!", end="\r\n");
+print("\r")
+
 if os.getenv("REQUEST_METHOD") == "POST":
     read = input()
     print("<h1>POST BODY = </h1>")
